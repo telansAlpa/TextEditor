c8b9d383be2be77a751525e4572e93fc60822342
Alpamys <telans.alpa@gmail.com>
2025-10-22 21:16:19 +0600

    Улутчения алгоритма чтения

--- AlgorithmRead.java
+++ AlgorithmRead.java
@@ -5,33 +5,77 @@ class AlgorithmRead{
 	static String[] format = {"java", "py"}; //for code()
     static List<Integer> linelist = new ArrayList<>(List.of(0)); //for volumeLine
 
-	//Разделяет весь написаный текст по строкам (в linelist)
+	//Записывает начало каждой строки как индекс, (в linelist)
 	void volumeLine(String text, int currentLine){
-        int n = 0;
+        int n = 0; // /n
+        int remove_up = 0; //Переменная в которой будем хронить сколько шагов оталкиватся
         System.out.println(currentLine);
 
         //Если новая строка снизу
-        for(int i = (linelist.size()-1); i < currentLine; i++){
-            linelist.add(text.length());
+        int linesize = linelist.size();//Сохраняем чтобы не изменялась при add
+        for(int i = (linesize-1); i < currentLine ; i++){
+
+            //Если было вставленно несколько строк
+            if(linesize<currentLine){
+                //Упорядочивания вставленных строк
+                for(int j = linelist.get(i);j < text.length()-1; j++){
+                    if(text.charAt(j) == '\n'){
+                        linelist.add(j+1);
+                        break;
+                    }
+                }
+            }
+            //Если одна строка
+            else{
+                linelist.add(text.length());
+            }
+            System.out.println("add..");
         }
+
+
+        //Если изменения строки с середины (более оптемезированое)
+        /*linesize = linelist.size();//Задаём новое значения, работа серединый
+        if(linesize > 2 && currentLine != linesize - 1){
+            for(int i = linelist.get(currentLine-1); i < linelist.get(currentLine+1); i++){
+                System.out.println(i);
+            }
+        }*/
         //Если изменения строки с середины
-        for(int i = linelist.get((currentLine)); i < text.length()-1; i++)// text.langth - 1, для выравнивания с индексом
-        {
-            System.out.println("Start: " + i);
+        linesize = linelist.size();//Задаём новое значения, работа серединый
+        for(int i = linelist.get(currentLine); i < text.length()-1; i++){
+
             if(text.charAt(i) == '\n'){
 
                 n++;
-                if(n+currentLine <= linelist.size() -1){
+                if(n+currentLine <= linesize -1){
                     linelist.set(n+currentLine, i+1);
                 }
                 else{//Если новая строка в середине
                     linelist.add(i+1);
-                    System.out.println("add");
+
+                    //Если было вставленно несколько строк
+                    if(n+currentLine == linesize+1){
+                        linelist.remove(currentLine);//Удоляем эту строку
+                    }
+                    System.out.println("add: " + i);
+                    remove_up++;
                 }
                 //
             }
         }
-        //Если удаления строки
+        System.out.println(linelist);
+        //Если вставленно несколько строк в середине
+        int nabarot = remove_up;
+        for(int i = linelist.get( currentLine); i > linelist.get(currentLine - remove_up); i--){
+            System.out.println("End: " + i);
+            if(text.charAt(i-1) == '\n'){
+
+                linelist.set(currentLine - (remove_up - nabarot), i); //ошибка здесь мы идем набарот, не варю
+                nabarot--;
+            }
+        }
+        System.out.println(linelist);
+        //Если пройсходит удаления строки
         for(int i = linelist.size(); i > currentLine + n +1; i--){
             linelist.remove(i-1);
         }
